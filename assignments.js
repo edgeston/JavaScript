eafcbc9bed3ae2558c39d6f64e9c39464f690418
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-02-09 22:55:16 -0500

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,9 @@
+/*LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console*/
+
+let country = 'United States'; 
+let continent = 'North America';
+let population = 329000000;
+console.log(country, continent, population);
