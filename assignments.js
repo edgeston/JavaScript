21de74dd55b2f2defb85728af1215b9279e2fe8d
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-02-14 19:55:53 -0600

    Update assignments.js

--- assignments.js
+++ assignments.js
@@ -67,3 +67,15 @@ console.log(country + " is in " + continent + ", and it's " + population + " spe
 // 1. Recreatethe'description'variablefromthelastassignment,thistime using the template literal syntax
 const description = `The ${country} is in ${continent}, and it's ${population} people speak ${language}.`;
 console.log(description);
+
+
+// LECTURE: Taking Decisions: if / else Statements
+// 1. Ifyourcountry'spopulationisgreaterthat33million,logastringlikethistothe console: 'Portugal's population is above average'. Otherwise, log a string like 'Portugal's population is 22 million below average' (the 22 is the average of 33 minus the country's population)
+// 2. Aftercheckingtheresult,changethepopulationtemporarilyto13andthento 130. See the different results, and set the population back to original
+
+
+if (population >= 330000) {
+    console.log(`The ${country}'s population is above average ${population}`);
+} else {
+    console.log(`The ${country}'s population is below average ${population}`);
+}
