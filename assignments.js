f526ffda8469510bdf5b7f76865a601eef3ddad6
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-02-17 13:46:14 -0600

    Add files via upload

--- assignments.js
+++ assignments.js
@@ -100,16 +100,40 @@ console.log(5 + 6 + '4' + 9 - 4 - 2); //1143
 // 7. Finally, convert 'numNeighbours' to a number, and watch what happens now
 // when you input 1
 // 8. Reflect on why we should use the === operator and type conversion in this
-// situation
+// // situation
 
-const numNeighbours = prompt(`How many neighboring countries does the ${country} have?`);
-console.log(typeof (numNeighbours));
+// const numNeighbours = prompt(`How many neighboring countries does the ${country} have?`);
+// console.log(typeof (numNeighbours));
 
-if (numNeighbours != 1) {
-    console.log(`One border needed`)
-} else if (numNeighbours > 1) {
-    console.log(`${numNeighbours} borders needed`)
-} else {
-    console.log(`No borders needed`)
-}
+// // if (numNeighbours != 1) {
+// //     console.log(`One border needed`)
+// // } else if (numNeighbours > 1) {
+// //     console.log(`${numNeighbours} borders needed`)
+// // } else {
+// //     console.log(`No borders needed`)
+// }
 //No Type Coercion performed on the Strict Equality/ InEquality Operator
+
+// LECTURE: Logical Operators
+// 1. Comment out the previous code so the prompt doesn't get in the way
+// 2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+// country that speaks english, has less than 50 million people and is not an
+// island.
+// 3. Write an if statement to help Sarah figure out if your country is right for her.
+// You will need to write a condition that accounts for all of Sarah's criteria. Take
+// your time with this, and check part of the solution if necessary.
+// 4. If yours is the right country, log a string like this: 'You should live in Portugal :)'.If
+// not, log 'Portugal does not meet your criteria :('
+// 5. Probably your country does not meet all the criteria.So go back and temporarily
+// change some variables in order to make the condition true(unless you live in
+//     Canada : D)
+
+country = 'Canada';
+language = 'English';
+population = 40000000;
+
+if (language === 'English' && population < 50000000 && !isIsland) {
+    console.log(`${country} is the country for you.`)
+} else {
+    console.log(`${country} is not for you.`)
+}
