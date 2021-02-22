10ee8bdae946a319a0f3128951dc91dd68534619
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-02-22 11:24:02 -0600

    Add files via upload

--- assignments.js
+++ assignments.js
@@ -161,3 +161,33 @@ const describeCanada = describeCountry('Canada', 37.6, 'Ottowa');
 console.log(describeCanada);
 const describeMexico = describeCountry('Mexico', 127.6, 'Mexico City');
 console.log(describeMexico);
+
+
+// LECTURE: Function Declarations vs. Expressions
+// 1. The world population is 7900 million people. Create a function declaration
+// called 'percentageOfWorld1' which receives a 'population' value, and
+// returns the percentage of the world population that the given population
+// represents. For example, China has 1441 million people, so it's about 18.2% of
+// the world population
+// 2. To calculate the percentage, divide the given 'population' value by 7900
+// and then multiply by 100
+// 3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
+// store the results into variables, and log them to the console
+// 4. Create a function expression which does the exact same thing, called
+// 'percentageOfWorld2', and also call it with 3 country populations (can be
+// the same populations)
+
+function percentageOfWorld1(country, population) {
+    return `${country} has ${population} million people, so that is about ${((population / 7900) * 100).toFixed(2)}% of the population`;
+}
+console.log(percentageOfWorld1('China', 1441));
+console.log(percentageOfWorld1('United States', 329));
+console.log(percentageOfWorld1('Canada', 35.6));
+
+const percentageOfWorld2 = function (country, population) {
+    return `${country} has ${population} million people, so that is about ${((population / 7900) * 100).toFixed(2)}% of the population`;
+}
+
+console.log(percentageOfWorld2('China', 1441));
+console.log(percentageOfWorld2('United States', 329));
+console.log(percentageOfWorld2('Canada', 35.6));
