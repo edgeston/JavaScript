50926508a5cbe503f68814018bac624a976a8d45
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-02-22 12:33:40 -0600

    Add files via upload

--- assignments.js
+++ assignments.js
@@ -202,3 +202,18 @@ console.log(percentageOfWorld3('China', 1441));
 console.log(percentageOfWorld3('United States', 329));
 console.log(percentageOfWorld3('Canada', 35.6));
 
+// LECTURE: Functions Calling Other Functions
+// 1. Create a function called 'describePopulation'. Use the function type you
+// like the most. This function takes in two arguments: 'country' and
+// 'population', and returns a string like this: 'China has 1441 million people,
+// which is about 18.2% of the world.'
+// 2. To calculate the percentage, 'describePopulation' call the
+// 'percentageOfWorld1' you created earlier
+// 3. Call 'describePopulation' with data for 3 countries of your choice
+
+function describePopulation(country, population) {
+    return `${country} has ${population} million people, which is about ${percentageOfWorld1}% of the wor'd population`;
+}
+
+const percentage = describePopulation('United States', 329);
+console.log(percentage);
