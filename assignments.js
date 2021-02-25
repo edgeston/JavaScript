0b6cf22d674f89177a9e1c1bce013df72b8c040d
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-02-25 09:57:51 -0600

    Add files via upload

--- assignments.js
+++ assignments.js
@@ -241,4 +241,17 @@ const percentages = [
 
 console.log(percentages);
 
+// LECTURE: Introduction to Objects
+// 1. Create an object called 'myCountry' for a country of your choice, containing
+// properties 'country', 'capital', 'language', 'population' and
+// 'neighbours' (an array like we used in previous assignments)
 
+const myCountry = {
+    country: 'United States',
+    capital: 'Washington DC',
+    language: 'English',
+    population: 329000000,
+    neighbors: ['Mexico', 'Canada']
+};
+
+console.log(myCountry);
