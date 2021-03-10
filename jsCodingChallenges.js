49d8af48f698a3bc7ff7ad8c19b62a8ba584128c
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-03-10 12:52:27 -0600

    Add files via upload

--- jsCodingChallenges.js
+++ jsCodingChallenges.js
@@ -333,13 +333,27 @@ function calcTip(bill) {
     }
 }
 
+
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
 
-for (let i = 0; i <= bills.length; i++) {
+
+for (let i = 0; i < bills.length; i++) {
     tips.push(calcTip(bills[i]));
     totals.push(bills[i] + tips[i]);
 }
+console.log(bills)
 console.log(tips);
 console.log(totals);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        // sum = sum + arr[i];
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
