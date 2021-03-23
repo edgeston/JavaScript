2edf53e81b8ceb22a89215d385711341180a2aff
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-03-22 22:58:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ function calcAge(birthYear) { // Defined in the Global Scope
     const age = 2021 - birthYear;
 
     function printAge() {
-        const output = `${firstName} is ${age} years old born in ${birthYear}` // Variable look up ascends
+        let output = `${firstName} is ${age} years old born in ${birthYear}` // Variable look up ascends
         console.log(output);
 
 
         if (birthYear >= 1981 && birthYear <= 1996) {
+            //Creating new variable with same name as outer scopes variable 
             const firstName = 'Monique'; // must initalize before running string JS loos for Variable name within current scope
             const str = `Oh, and you're a millenial, ${firstName}`;
             console.log(str);
@@ -16,6 +17,8 @@ function calcAge(birthYear) { // Defined in the Global Scope
                 return a + b;
             }
             console.log(add(3, 4)); //Code still runs outside the blockscope
+            output = 'NEW OUTPUT'; //CRe-assigning outer scope's variable 
+            console.log(output);
         }
         // console.log(str); //const and let variables are only available in the block scope
     }
