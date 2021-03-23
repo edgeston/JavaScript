5545223e04d03eb9bf01c74f7bcb22f303a2466b
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-03-22 22:50:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+function calcAge(birthYear) { // Defined in the Global Scope
+    const age = 2021 - birthYear;
+
+    function printAge() {
+        const output = `${firstName} is ${age} years old born in ${birthYear}` // Variable look up ascends
+        console.log(output);
+
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            const firstName = 'Monique'; // must initalize before running string JS loos for Variable name within current scope
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+            console.log(add(3, 4)); //Code still runs outside the blockscope
+        }
+        // console.log(str); //const and let variables are only available in the block scope
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'Monikka'; // Global variable = Accessible Everywhere
+calcAge(1993);
+// console.log(age); // Reference Error: age is not defines globally
+// printAge(); // function cannot definined globally
